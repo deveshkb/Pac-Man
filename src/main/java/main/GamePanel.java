0c8e47619abea8739e0fcaecf92f7a9fde6ba1cf
DevKBoost <dkb020102@gmail.com>
2026-09-05 21:21:20 +0400

    Frame&GameLoop_v1

---
+++ src/main/java/main/GamePanel.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author bhudy
+ */
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+    
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+    
+    final int tileSize = originalTileSize * scale; //48x46 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    final int screenHeight = tileSize * maxScreenRow; //576 pixels
+    
+    Thread gameThread;
+    
+    
+    public GamePanel(){
+     
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        
+    }
+
+    public void startGameThread(){
+    
+        gameThread = new Thread();
+        gameThread.start();
+    }
+    
+    @Override
+    public void run() {
+        
+            while(gameThread != null) {
+                
+                System.out.println("Test");
+                
+            }
+        
+        }
+   
+    }
