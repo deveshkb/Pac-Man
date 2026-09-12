b0e2cdc7e88781a411ceed924cff64c392304fdd
Devesh Kumar Bhudye <dkb020102@gmail.com>
2026-09-12 19:13:01 +0400

    Fixed GameThread issue and added 60fps game loop

--- GamePanel.java
+++ GamePanel.java
@@ -4,43 +4,58 @@ import java.awt.Dimension;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable{
-    
-    // SCREEN SETTINGS
+
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 3;
-    
+
     final int tileSize = originalTileSize * scale; //48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow; //576 pixels
-    
+
+    final int FPS = 60;
+
     Thread gameThread;
-    
-    
+
     public GamePanel(){
-     
+
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
-        
+
     }
 
     public void startGameThread(){
-    
-        gameThread = new Thread();
+
+        gameThread = new Thread(this);
         gameThread.start();
     }
-    
+
     @Override
     public void run() {
         
-            while(gameThread != null) {
-                
-                System.out.println("Test");
-                
+        double drawInterval = 1000000000.0 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
             }
-        
         }
-   
     }
+
+    public void update() {
+        // TODO: game logic goes here
+    }
+
+}
